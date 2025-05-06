815f8528864162a4750a25cfcadd72f78564848c
Lucy <lucymidgley13@gmail.com>
2025-05-06 15:38:19 +0900

    fix camel case inconsistencies

--- backend/src/helpers/helpers.ts
+++ backend/src/helpers/helpers.ts
@@ -1,19 +1,19 @@
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
 import * as dotenv from "dotenv";
-import { payload } from "../dto/user.dto";
+import { UserResponse } from "../dto/user";
 
 dotenv.config();
 const { JWT_SECRET = "" } = process.env;
 export class encrypt {
-  static async encryptpass(password: string) {
+  static async encryptPassword(password: string) {
     return bcrypt.hashSync(password, 12);
   }
-  static comparepassword(hashPassword: string, password: string) {
+  static comparePassword(hashPassword: string, password: string) {
     return bcrypt.compareSync(password, hashPassword);
   }
 
-  static generateToken(payload: payload) {
+  static generateToken(payload: UserResponse) {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
   }
 }
