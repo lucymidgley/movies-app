3869fc885de573ab8b640d07c0445b6ecb065707
Lucy <lucymidgley13@gmail.com>
2025-05-06 16:15:17 +0900

    add movie migration

---
+++ backend/migrations/1746515584369-movies.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Movies1746515584369 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            --Table Definition
+            CREATE TABLE "movies"  (
+               "id" bigint GENERATED ALWAYS AS IDENTITY,
+                "title" character varying NOT NULL,
+                "description" character varying NOT NULL,
+                "director" character varying NOT NULL,
+                "year" integer NOT NULL,
+                "rating" character varying NOT NULL,
+                "image" character varying NOT NULL,
+                "cast" character varying NOT NULL,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+              )
+              `),
+      undefined;
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "movies"`, undefined);
+  }
+}
