74cb0838d4c414edf9ebe187b18282a1b7a2dfa8
Lucy <lucymidgley13@gmail.com>
2025-05-06 15:52:18 +0900

    add more routes to user controller

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -3,6 +3,7 @@ import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
 import { encrypt } from "../helpers/helpers";
 import { UserResponse } from "../dto/user";
+import * as cache from "memory-cache";
 
 export class UserController {
   static async signup(req: Request, res: Response) {
@@ -25,5 +26,45 @@ export class UserController {
     return res
       .status(200)
       .json({ message: "User created successfully", token, userDataSent });
+  }
+  static async getUsers(req: Request, res: Response) {
+    const data = cache.get("data");
+    if (data) {
+      console.log("serving from cache");
+      return res.status(200).json({
+        data,
+      });
+    } else {
+      console.log("serving from db");
+      const userRepository = AppDataSource.getRepository(User);
+      const users = await userRepository.find();
+
+      cache.put("data", users, 6000);
+      return res.status(200).json({
+        data: users,
+      });
+    }
+  }
+  static async updateUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOne({
+      where: { id: Number(id) },
+    });
+    user.name = name;
+    user.email = email;
+    await userRepository.save(user);
+    res.status(200).json({ message: "update", user });
+  }
+
+  static async deleteUser(req: Request, res: Response) {
+    const { id } = req.params;
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOne({
+      where: { id: Number(id) },
+    });
+    await userRepository.remove(user);
+    res.status(200).json({ message: "ok" });
   }
 }
