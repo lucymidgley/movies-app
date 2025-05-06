10efe0e1de5e8c741042caeb78ddb75c0fe97d2b
lucymidgley <lucymidgley13@gmail.com>
2025-05-06 20:43:15 +0900

    add bash helpers file

---
+++ bash_helpers.sh
@@ -0,0 +1,6 @@
+alias dcapi='docker compose run --rm backend bash'
+alias dcm='docker compose run --rm backend npm run migration'
+alias dcu='docker compose up'
+alias dcb='docker compose build'
+alias ..='cd ..'
+git config --global alias.shove 'push -u origin HEAD'
