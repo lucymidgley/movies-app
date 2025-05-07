e581c8fe093bacbb91d73c0a94023b14e06037f4
lucymidgley <lucymidgley13@gmail.com>
2025-05-07 19:38:58 +0900

    add scaffold for login page

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,12 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
+    element: (
+      <PrivateRoute>
+        <HomePage />
+      </PrivateRoute>
+    ),
   },
+  {
+    path: '/login',
+    element: <h1>Login!</h1>
+  }
 ]);
+
+function PrivateRoute({ children }: any) {
+  const isAuthenticated = true;
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
+
 
 export function Router() {
   return <RouterProvider router={router} />;
