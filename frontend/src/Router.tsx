d12b23af78f437a319d98592d6e78040c607ea95
lucymidgley <lucymidgley13@gmail.com>
2025-05-07 21:09:13 +0900

    add mantine ui for login and register

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { SignIn } from './pages/SignIn/SignIn';
+import { Register } from './pages/Register/Register';
 
 const router = createBrowserRouter([
   {
@@ -12,12 +14,16 @@ const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <h1>Login!</h1>
+    element: <SignIn />
+  },
+  {
+    path: '/register',
+    element: <Register />
   }
 ]);
 
 function PrivateRoute({ children }: any) {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
   return isAuthenticated ? children : <Navigate to="/login" />;
 }
 
