ed861a5d82f39af28b69809927f99286f37acc03
lucymidgley <lucymidgley13@gmail.com>
2025-05-08 21:01:52 +0900

    get sign in and register pages working

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -2,89 +2,36 @@ import {
     Anchor,
     Button,
     Checkbox,
+    Container,
     Group,
     Paper,
-    PaperProps,
     PasswordInput,
-    Stack,
+    Text,
     TextInput,
+    Title,
   } from '@mantine/core';
-  import { useForm } from '@mantine/form';
-  import { upperFirst, useToggle } from '@mantine/hooks';
-  import classes from './Register.module.css';
-
+  import classes from '../SignIn/SignIn.module.css';
+import { Link } from 'react-router-dom';
   
-  export function Register(props: PaperProps) {
-    const [type, toggle] = useToggle(['login', 'register']);
-    const form = useForm({
-      initialValues: {
-        email: '',
-        name: '',
-        password: '',
-        terms: true,
-      },
-  
-      validate: {
-        email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
-        password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
-      },
-    });
-  
+  export function Register() {
     return (
-      <Paper radius="md" p="lg" withBorder {...props}>
+      <Container size={420} my={40}>
+        <Title ta="center" className={classes.title}>
+          Welcome!
+        </Title>
   
-        <form onSubmit={form.onSubmit(() => {})} className={classes.form}>
-          <Stack>
-            {type === 'register' && (
-              <TextInput
-                label="Name"
-                placeholder="Your name"
-                value={form.values.name}
-                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
-                radius="md"
-              />
-            )}
+        <Text className={classes.subtitle}>
+          Already have an account? <Anchor component={Link} to="/login">Sign in</Anchor>
+        </Text>
   
-            <TextInput
-              required
-              label="Email"
-              placeholder="hello@mantine.dev"
-              value={form.values.email}
-              onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-              error={form.errors.email && 'Invalid email'}
-              radius="md"
-            />
-  
-            <PasswordInput
-              required
-              label="Password"
-              placeholder="Your password"
-              value={form.values.password}
-              onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-              error={form.errors.password && 'Password should include at least 6 characters'}
-              radius="md"
-            />
-  
-            {type === 'register' && (
-              <Checkbox
-                label="I accept terms and conditions"
-                checked={form.values.terms}
-                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
-              />
-            )}
-          </Stack>
-  
-          <Group justify="space-between" mt="xl">
-            <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-              {type === 'register'
-                ? 'Already have an account? Login'
-                : "Don't have an account? Register"}
-            </Anchor>
-            <Button type="submit" radius="xl">
-              {upperFirst(type)}
-            </Button>
-          </Group>
-        </form>
-      </Paper>
+        <Paper withBorder shadow="sm" p={22} mt={30} radius="md">
+          <TextInput label="Name" placeholder="Jane Smith" required radius="md" />
+          <TextInput label="Email" placeholder="your@email.com" required radius="md" />
+          <PasswordInput label="Password" placeholder="Your password" required mt="md" radius="md" />
+          <Button fullWidth mt="xl" radius="md">
+            Sign up
+          </Button>
+        </Paper>
+      </Container>
     );
   }
--- frontend/src/pages/SignIn/SignIn.tsx
+++ frontend/src/pages/SignIn/SignIn.tsx
@@ -11,6 +11,7 @@ import {
     Title,
   } from '@mantine/core';
   import classes from './SignIn.module.css';
+import { Link } from 'react-router-dom';
   
   export function SignIn() {
     return (
@@ -20,15 +21,15 @@ import {
         </Title>
   
         <Text className={classes.subtitle}>
-          Do not have an account yet? <Anchor>Create account</Anchor>
-        </Text>
+          Do not have an account yet? <Anchor component={Link} to="/register">Create account</Anchor>
+        </Text> 
   
         <Paper withBorder shadow="sm" p={22} mt={30} radius="md">
           <TextInput label="Email" placeholder="you@mantine.dev" required radius="md" />
           <PasswordInput label="Password" placeholder="Your password" required mt="md" radius="md" />
           <Group justify="space-between" mt="lg">
             <Checkbox label="Remember me" />
-            <Anchor component="button" size="sm">
+            <Anchor component={Link} size="sm" to="/forgot-password">
               Forgot password?
             </Anchor>
           </Group>
